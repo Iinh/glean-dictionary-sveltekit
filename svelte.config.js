2e6db370d12ceb73658dae74b4626cb07f078cc1
Linh Nguyen <linhnguyen@mozilla.com>
2021-05-07 18:11:09 -0700

    more wip

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ const sveltePreprocess = require('svelte-preprocess');
 const node = require('@sveltejs/adapter-node');
 const pkg = require('./package.json');
 const netlify = require('@sveltejs/adapter-netlify')
-const ssr = require('@sveltejs/adapter-static')
+// const ssr = require('@sveltejs/adapter-static')
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
